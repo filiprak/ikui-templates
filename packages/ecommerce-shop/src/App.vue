5f15d05d33c19da9140cbcccf4424df73b252ddb
Filip Rak <filip.rak@ikol.com>
2026-01-17 14:33:28 +0100

    Cleanup [skip ci]

---
+++ packages/ecommerce-shop/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <IkButton design="primary"
+                  icon="arrow-left">
+            Button
+        </IkButton>
+    </div>
+</template>
+<script setup lang="ts">
+import { IkButton } from '@ikol/ui-kit/components/IkButton';
+</script>
